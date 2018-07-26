1da60bee18b96b96346864bba862e527ca339c47
Robert Stefanic <rstefanic72@gmail.com>
2018-07-25 23:12:57 -0500

    fixed indenting w/ install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,13 @@ nix_path=/etc/nixos/
 
 xmonad_config() {
     # If xmonad is installed, then set up xmonad settings
-    if test ! $(which xmonad); then
+    if test ! $(which xmobar); then
+        if [ -e ~/.xmobarrc ]; then
+	          mv ~/.xmobarrc $dir_backup
+	          rm ~/.xmobarrc
+	      fi
 
-	if [ -e ~/.xmobarrc ]; then
-	    mv ~/.xmobarrc $dir_backup
-	    rm ~/.xmobarrc
-	fi
-
-	cp .xmobarrc ~/
+	      cp .xmobarrc ~/
     fi
 }
 
@@ -35,12 +34,12 @@ vim_config() {
 emacs_config() {
     # If emacs is installed, setup emacs files
     if test ! $(which emacs); then
-	if [ -e ~/.emacs ]; then
-	    mv ~/.emacs $dir_backup
-	    rm ~/.emacs
-	fi
+	      if [ -e ~/.emacs ]; then
+	          mv ~/.emacs $dir_backup
+	          rm ~/.emacs
+	      fi
 
-	cp .emacs ~/
+	      cp .emacs ~/
     fi
 }
 
@@ -53,27 +52,27 @@ create_backup_dotfiles() {
 install_zsh() {
     # Check if zsh is installed
     if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-	if [ ! -d $dir/oh-my-zsh/ ]; then
-	    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	fi
+	      if [ ! -d $dir/oh-my-zsh/ ]; then
+	          sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	      fi
 
-	# Set zsh as the default shell
-	if [ ! $(echo $SHELL) = $(which zsh) ]; then
-	    sudo chsh -s $(which zsh)
-	fi
+        # Set zsh as the default shell
+	      if [ ! $(echo $SHELL) = $(which zsh) ]; then
+	          sudo chsh -s $(which zsh)
+	      fi
+    
+	      if [ -f ~/.zshrc ]; then
+	          mv -f ~/.zshrc $dir_backup
+	      fi
+	  
+	      cp -f .zshrc ~/.zshrc
 
-	if [ -f ~/.zshrc ]; then
-	    mv -f ~/.zshrc $dir_backup
-	fi
-	
-	cp -f .zshrc ~/.zshrc
-    else
-	if [ -f /etc/debian_version ]; then
-	    sudo apt-get install zsh
-	    install_zsh
-	else
-	    echo "Cannot install zsh -- Cannot use apt-get to install zsh"
-	fi
+	      if [ -f /etc/debian_version ]; then
+	          sudo apt-get install zsh
+	          install_zsh
+	      else
+	          echo "Cannot install zsh -- Cannot use apt-get to install zsh"
+	      fi
     fi
 }
 
