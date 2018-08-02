663b9ba4acc8e6a478f33cf2753c6f30dbff88fc
Robert Stefanic <rstefanic72@gmail.com>
2018-08-01 20:52:51 -0500

    added init-channels script

---
+++ init-channels.sh
@@ -0,0 +1,3 @@
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
+nix-channel --add https://nixos.org/channels/nixos-17.09 nixos
+nix-channel --add https://nixos.org/channels/nixos-18.03 nixos-18.03
