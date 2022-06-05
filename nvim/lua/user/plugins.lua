d626126e089ed824a950728e81bb531144a4cc99
Robert Stefanic <rstefanic72@gmail.com>
2022-06-04 21:39:05 -0400

    nvim: Add treesitter plugin
    Added treesitter plugin and installed several languages for syntax
    highlighting.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -12,8 +12,16 @@ Plug('kyazdani42/nvim-web-devicons');
 Plug('nvim-telescope/telescope-fzf-native.nvim', { run = 'make' });
 Plug('nvim-telescope/telescope-live-grep-raw.nvim');
 Plug('nvim-telescope/telescope.nvim');
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' });
 
 vim.call('plug#end')
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "cpp", "php", "css", "html", "vue" },
+    highlight = {
+        enable = true
+    }
+}
 
 vim.cmd('colorscheme gruvbox')
 
