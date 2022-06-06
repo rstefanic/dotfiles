4f35e51f5489719fd3efa3a8eefdc11bbb96cce0
Robert Stefanic <rstefanic72@gmail.com>
2022-06-06 09:34:24 -0400

    nvim options: Add hidden and reoganize tab options

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,10 +1,12 @@
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
+vim.o.hidden = true
 vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 vim.o.number = true
 vim.o.termguicolors = true
 vim.o.undofile = true
--- vim.o.spell = true
 vim.o.title = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
