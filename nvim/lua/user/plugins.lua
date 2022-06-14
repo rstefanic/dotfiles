218b87026fe1711f0380fef11c29f482c0d71aa6
Robert Stefanic <rstefanic72@gmail.com>
2022-06-14 17:32:29 -0400

    nvim plugins: Install nvim-treesitter-context

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -12,8 +12,10 @@ Plug('kyazdani42/nvim-web-devicons');
 Plug('nvim-telescope/telescope-fzf-native.nvim', { run = 'make' });
 Plug('nvim-telescope/telescope-live-grep-raw.nvim');
 Plug('nvim-telescope/telescope.nvim');
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' });
 Plug('kyazdani42/nvim-web-devicons');
+
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' });
+Plug('nvim-treesitter/nvim-treesitter-context');
 
 Plug('yaegassy/coc-intelephense', { ['do'] = 'yarn install --frozen-lockfile' });
 
@@ -34,5 +36,20 @@ require'nvim-treesitter.configs'.setup {
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+require'treesitter-context'.setup{
+    enable = true,
+    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        -- For all filetypes
+        -- Note that setting an entry here replaces all other patterns for this entry.
+        -- By setting the 'default' entry below, you can control which nodes you want to
+        -- appear in the context window.
+        default = {
+            'class',
+            'function',
+            'method',
+        },
+    },
+}
 
 vim.cmd[[colorscheme tokyonight]]
