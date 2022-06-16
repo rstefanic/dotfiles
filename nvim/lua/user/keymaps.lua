906f24a26cdf2029e4318851c963d55c299e3489
Robert Stefanic <rstefanic72@gmail.com>
2022-06-16 08:22:32 -0400

    nvim keymaps: Add binding to explore current file directory

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -52,5 +52,7 @@ keymap('i', '<C-k>', '<Esc>:move .-2<CR>==gi')
 keymap('x', '<C-j>', ":move '>+1<CR>gv-gv")
 keymap('x', '<C-k>', ":move '<-2<CR>gv-gv")
 
+keymap('n', '<leader>pf', ':Ex<CR>');
+
 -- Ripgrep search
 keymap('n', '<leader>rg', ":!rg -i")
