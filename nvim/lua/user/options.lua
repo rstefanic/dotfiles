086e06300d5d94d4f5ca5545aeb229b1d079432d
Robert Stefanic <rstefanic72@gmail.com>
2022-06-29 09:25:38 -0400

    nvim options: Reorganize options and replace vim.o with vim.opt

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,31 +1,41 @@
-vim.o.hidden = true
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.number = true
-vim.o.termguicolors = true
-vim.o.undofile = true
-vim.o.title = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.wildmode = 'longest:full,full'
-vim.o.wrap = false
-vim.o.list = true
-vim.o.listchars = 'tab:▸ ,trail:·'
-vim.o.mouse = 'a'
-vim.o.splitright = true
-vim.o.splitbelow = true
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
-vim.o.clipboard = 'unnamedplus'
-vim.o.confirm = true
-vim.o.backup = false
-vim.o.swapfile = false
-vim.o.updatetime = 250          -- Decrease CursorHold delay
-vim.o.redrawtime = 10000        -- Allow more time for loading syntax on large files
-vim.o.showmode = false
-vim.o.fillchars = 'eob: '
-vim.o.hlsearch = false
-vim.o.signcolumn = 'yes'
+-- General
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.hidden = true
+vim.opt.title = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.confirm = true
+vim.opt.updatetime = 250          -- Decrease CursorHold delay
+vim.opt.redrawtime = 10000        -- Allow more time for loading syntax on large files
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.showmode = false
+vim.opt.hlsearch = false
+vim.opt.signcolumn = 'yes'
+vim.opt.wildmode = 'longest:full,full'
+
+-- Tabs and what not
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.cindent = true
+vim.opt.breakindent = true
+
+-- Other text fun
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.list = true
+vim.opt.listchars = 'tab:▸ ,trail:·'
+vim.opt.fillchars = 'eob: '
+
+-- Backup, undo, and swap files
+vim.opt.undofile = true
+vim.opt.backup = false
+vim.opt.swapfile = false
