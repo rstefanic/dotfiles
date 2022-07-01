0972448817095b097f93786d371f50cf3d08319d
Robert Stefanic <rstefanic72@gmail.com>
2022-06-30 21:12:00 -0400

    nvim plugins: Now I'm back on gruvbox
    I should really just make these functions that I can call to change my
    style.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -62,7 +62,8 @@ vim.g.tokyonight_transparent = true
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.cmd[[colorscheme tokyonight]]
+vim.g.gruvbox_italics = 1
+vim.cmd[[colorscheme gruvbox]]
 
 -- Transparent Background
 vim.api.nvim_set_hl(0, "Normal", {
