e9e27870403b60ab9fc45e38ecc8dcb87a0e532a
Robert Stefanic <rstefanic72@gmail.com>
2022-12-08 15:14:45 -0500

    nvim color: Make background transparent
    Also switched to using the nvim api to set the colorscheme.

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -6,4 +6,5 @@ vim.api.nvim_set_hl(0, "Normal", {
 vim.g.gruvbox_invert_selection = 0
 vim.g.gruvbox_italicize_strings = 1
 
-vim.cmd("colorscheme gruvbox")
+vim.cmd.colorscheme("gruvbox");
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
