322b772f1641bb25bb5887ec94fcb100ab525c63
Robert Stefanic <rstefanic72@gmail.com>
2022-12-23 18:26:39 -0500

    nvim color: Add transparency function
    Added a function that can be called during a session to make the window
    transparent.

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -7,13 +7,17 @@ require('rose-pine').setup({
 })
 
 vim.cmd.colorscheme("rose-pine");
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
-vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2B79A0" })
-vim.api.nvim_set_hl(0, "CursorLineNR", { bg = "none" })
-vim.api.nvim_set_hl(0, "netrwDir", { bg = "none"})
+
+function transparent()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+    vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2B79A0" })
+    vim.api.nvim_set_hl(0, "CursorLineNR", { bg = "none" })
+    vim.api.nvim_set_hl(0, "netrwDir", { bg = "none"})
+end
+
