7c14eb488a3b202e347d192c73cf6e8042abc361
Robert Stefanic <rstefanic72@gmail.com>
2022-12-24 11:03:45 -0500

    install: Add link for .tmux.conf and install tpm

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ mkdir -p ~/.config/nvim
 ln -s $PWD/nvim/init.lua ~/.config/nvim/
 ln -s $PWD/nvim/lua/ ~/.config/nvim/
 ln -s $PWD/nvim/after/ ~/.config/nvim/
+ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
