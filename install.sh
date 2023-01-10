11c0f7f5c02c735361497afd9f8573ec09d29d95
Robert Stefanic <rstefanic72@gmail.com>
2023-01-09 22:11:29 -0500

    install: Link alacritty to ~/.config/alacritty

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -s $PWD/nvim/init.lua ~/.config/nvim/
 ln -s $PWD/nvim/lua/ ~/.config/nvim/
 ln -s $PWD/nvim/after/ ~/.config/nvim/
 ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
+ln -s $PWD/alacritty/ ~/.config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
