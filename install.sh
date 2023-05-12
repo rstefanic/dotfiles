d2394d85ba15a9ded687be28271c2ab452d9f453
Robert Stefanic <rstefanic72@gmail.com>
2023-05-12 16:42:42 -0400

    feat: replace alacritty with kitty

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ ln -s $PWD/nvim/init.lua ~/.config/nvim/
 ln -s $PWD/nvim/lua/ ~/.config/nvim/
 ln -s $PWD/nvim/after/ ~/.config/nvim/
 ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
-ln -s $PWD/alacritty/ ~/.config
+ln -s $PWD/kitty/ ~/.config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
