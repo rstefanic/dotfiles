257fded356bde5cf0df9f324d620cc6fb63489d2
Robert Stefanic <rstefanic72@gmail.com>
2024-08-10 11:05:11 -0400

    Project: Add Wezterm

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ ln -s $PWD/nvim/init.lua ~/.config/nvim/
 ln -s $PWD/nvim/lua/ ~/.config/nvim/
 ln -s $PWD/nvim/after/ ~/.config/nvim/
 ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
+ln -s $PWD/wezterm/.wezterm.lua ~/.wezterm.lua
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
