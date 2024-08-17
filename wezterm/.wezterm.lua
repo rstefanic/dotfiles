59da458df98957ca713ddba3451ac7db739cf613
Robert Stefanic <rstefanic72@gmail.com>
2024-08-17 14:27:07 -0400

    wezterm: Remove font setting and default cursor style
    The default font for wezterm is JetBrains Mono and it includes fall back
    fonts to handle Nerd Font icons and glphys.
    
    The default cursor style is already 'SteadyBlock'.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,10 +2,7 @@ local wezterm = require('wezterm')
 
 local config = wezterm.config_builder()
 config.enable_tab_bar = false;
-config.default_cursor_style = 'SteadyBlock'
 config.cursor_blink_rate = 0
-
-config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 16
 
 return config
