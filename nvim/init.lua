b1c787a77822df31fae6cd1f59aa30fddf6da4d8
Robert Stefanic <rstefanic72@gmail.com>
2024-09-05 21:03:46 -0400

    nvim: Install 'mrjones2014/smart-splits.nvim'
    Replaces 'christoomey/vim-tmux-navigator'.

--- nvim/init.lua
+++ nvim/init.lua
@@ -146,8 +146,8 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
-  -- Navigate easier with tmux
-  'christoomey/vim-tmux-navigator',
+
+  { 'mrjones2014/smart-splits.nvim' },
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -643,6 +643,16 @@ require('lspconfig').intelephense.setup({
   },
   capabilities = capabilities,
 })
+
+-- Setup Smart Splits
+vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
 
 -- Setup neovim lua configuration
 require('neodev').setup()
