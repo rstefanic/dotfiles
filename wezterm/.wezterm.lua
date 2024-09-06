c56012a9430b47445d27144c9e0ed37e127f1410
Robert Stefanic <rstefanic72@gmail.com>
2024-09-05 21:05:40 -0400

    wezterm: Bold font by default

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 config.enable_tab_bar = false;
 config.cursor_blink_rate = 0
 config.font_size = 16
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
 
 return config
