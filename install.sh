f402d88dc5d7f069f79e250b5f5ec1baae7931fa
Robert Stefanic <rstefanic72@gmail.com>
2024-12-26 16:56:31 -0500

    install: Add ghostty config installation

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s $PWD/nvim/init.lua ~/.config/nvim/
 ln -s $PWD/nvim/lua/ ~/.config/nvim/
 ln -s $PWD/nvim/after/ ~/.config/nvim/
 ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
+ln -s $PWD/ghostty ~/.config/ghostty
 ln -s $PWD/wezterm/.wezterm.lua ~/.wezterm.lua
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
