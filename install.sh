d98e5d3cade985b9d6a43ac5225af26776008f31
Robert Stefanic <rstefanic72@gmail.com>
2025-02-18 11:50:05 -0500

    Install: Link ghostty config if it's an argument

--- install.sh
+++ install.sh
@@ -5,7 +5,20 @@ ln -s $PWD/nvim/init.lua ~/.config/nvim/
 ln -s $PWD/nvim/lua/ ~/.config/nvim/
 ln -s $PWD/nvim/after/ ~/.config/nvim/
 ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
-ln -s $PWD/ghostty ~/.config/ghostty
 ln -s $PWD/.zshrc ~/.zshrc
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+while [ $# -ne 0 ]
+do
+    arg="$1"
+    case "$arg" in
+        --ghostty)
+            ln -s $PWD/ghostty ~/.config/ghostty
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
