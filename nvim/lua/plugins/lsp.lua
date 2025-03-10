55e40a3177f48eed67c4cc07fe4bf263695d2c54
Robert Stefanic <rstefanic72@gmail.com>
2025-03-10 09:22:34 -0400

    Neovim: Move 'volar' and 'ts_ls' configuration to `lsp.lua`

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,43 @@ return {
       require('lspconfig')['hls'].setup {
         filetypes = { 'haskell', 'lhaskell', 'cabal' }
       }
+
+      require('lspconfig').volar.setup {
+        filetypes = { 'vue ' },
+        init_options = {
+          vue = {
+            hybridMode = true,
+          },
+          typescript = {
+            --
+            tsdk = '/path/to/node_modules/typescript/lib'
+          }
+        },
+        -- Use a local server and fall back to a global TypeScript Server installation
+        on_new_config = function(new_config, new_root_dir)
+          local lib_path = vim.fs.find('node_modules/typescript/lib', { path = new_root_dir, upward = true })[1]
+          if lib_path then
+            new_config.init_options.typescript.tsdk = lib_path
+          end
+        end,
+      }
+
+      require('lspconfig').ts_ls.setup {
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+              languages = { "javascript", "typescript", "vue" },
+            },
+          },
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      }
     end
   },
 }
