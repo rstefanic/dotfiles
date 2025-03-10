c4e7e29c7520dfefaf825345c7563a320813f500
Robert Stefanic <rstefanic72@gmail.com>
2025-03-10 11:37:45 -0400

    Neovim: Disable lsp autoformat for 'ts_ls' and 'volar'

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -44,9 +44,7 @@ return {
           return
         end
 
-        -- Tsserver usually works poorly. Sorry you work with bad languages
-        -- You can remove this line if you know what you're doing :)
-        if client.name == 'tsserver' then
+        if client.name == 'ts_ls' or client.name == 'volar' then
           return
         end
 
