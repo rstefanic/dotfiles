2289d057ec65dad89c91ffabb78a531a4377f282
Robert Stefanic <rstefanic72@gmail.com>
2025-03-10 01:07:14 +0000

    Neovim: Edit comments in lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -11,6 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 
+-- Put lazy in the runtime path
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
@@ -126,12 +127,4 @@ require('lazy').setup({
   require 'plugins.autoformat',
   require 'plugins.git',
   require 'plugins.colorscheme',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
 }, {})
