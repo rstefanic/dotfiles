2f36cb296a17be72f72efc5b1a18e9091950e5ae
Robert Stefanic <rstefanic72@gmail.com>
2025-03-15 13:34:56 -0400

    Neovim: Move 'telescope' configuration to `telescope.lua`

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -56,26 +56,6 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim',        opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-  },
-
   require 'plugins.treesitter',
   require 'plugins.lsp',
   require 'plugins.autoformat',
@@ -85,5 +65,6 @@ require('lazy').setup({
   require 'plugins.which-key',
   require 'plugins.oil',
   require 'plugins.neoformat',
+  require 'plugins.telescope',
 
 }, {})
