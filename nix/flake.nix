cdb7c6b43ccc531acc5a93ec5f2ae2a244508822
Robert Stefanic <rstefanic72@gmail.com>
2025-03-15 13:17:44 -0400

    nix: Use nix to manage MacOS packages

---
+++ nix/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "nix-darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      nixpkgs.config.allowUnfree = true;
+
+      environment.systemPackages =
+        [ pkgs.docker
+          pkgs.fzf
+          pkgs.neovim
+          pkgs.ngrok
+          pkgs.ripgrep
+          pkgs.tmux
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    darwinConfigurations."m3" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
