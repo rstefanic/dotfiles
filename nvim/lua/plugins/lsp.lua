bc48fad15a96140e697e8212805c65613586de5e
Robert Stefanic <rstefanic72@gmail.com>
2025-03-16 13:02:47 -0400

    Neovim: Move 'mason-lspconfig' to `config` setup for lazy.nvim
    While putting the setup function for 'neovim/nvim-lspconfig' works, it
    feels odd since `opts` should really be a table. I prefer this as it's a
    setup for 'mason-lspconfig' which is setup as a dependency before
    'nvim-lspconfig' is configured.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,44 +40,46 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+          local capabilities = vim.lsp.protocol.make_client_capabilities()
+          capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+          -- mason-lspconfig requires that these setup functions are called in this order
+          -- before setting up the servers.
+          require('mason').setup()
+          require('mason-lspconfig').setup()
+
+          -- Setup neovim lua configuration
+          require('neodev').setup()
+
+          -- Ensure the servers above are installed
+          local mason_lspconfig = require 'mason-lspconfig'
+
+          local servers = {
+            clangd = {},
+          }
+
+          mason_lspconfig.setup {
+            ensure_installed = vim.tbl_keys(servers),
+          }
+
+          mason_lspconfig.setup_handlers {
+            function(server_name)
+              require('lspconfig')[server_name].setup {
+                capabilities = capabilities,
+                on_attach = require('plugins.lsp').on_attach,
+                settings = servers[server_name],
+                filetypes = (servers[server_name] or {}).filetypes,
+              }
+            end,
+          }
+        end
+      },
       { 'j-hui/fidget.nvim',       opts = {} },
       'folke/neodev.nvim',
     },
-    opts = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-      -- mason-lspconfig requires that these setup functions are called in this order
-      -- before setting up the servers.
-      require('mason').setup()
-      require('mason-lspconfig').setup()
-
-      -- Setup neovim lua configuration
-      require('neodev').setup()
-
-      -- Ensure the servers above are installed
-      local mason_lspconfig = require 'mason-lspconfig'
-
-      local servers = {
-        clangd = {},
-      }
-
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(servers),
-      }
-
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = require('plugins.lsp').on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-          }
-        end,
-      }
-    end,
     config = function()
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
