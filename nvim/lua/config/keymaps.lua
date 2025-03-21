d75240931297d568b29ffb7bf154cc4755a99dd9
Robert Stefanic <rstefanic72@gmail.com>
2025-03-21 09:13:00 -0400

    keymaps: Remove unused `Move text up and down` keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -42,13 +42,5 @@ keymap('n', 'sv', ':vsplit<Return><C-w>w')
 keymap('n', 'sc', '<C-w>c')
 keymap('n', 'so', '<C-w>o')
 
--- Move text up and down
-keymap('n', '<C-j>', ':move .+1<CR>==')
-keymap('n', '<C-k>', ':move .-2<CR>==')
-keymap('i', '<C-j>', '<Esc>:move .+1<CR>==gi')
-keymap('i', '<C-k>', '<Esc>:move .-2<CR>==gi')
-keymap('x', '<C-j>', ":move '>+1<CR>gv-gv")
-keymap('x', '<C-k>', ":move '<-2<CR>gv-gv")
-
 -- Explorer
 keymap('n', '<leader>pf', ':Ex<CR>');
