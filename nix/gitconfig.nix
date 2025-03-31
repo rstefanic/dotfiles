5ba9c910d75476c04b572d28b74d0720b4c06941
Robert Stefanic <rstefanic72@gmail.com>
2025-03-30 21:01:05 -0400

    nix/gitconfig: Deeply merge to attribute sets
    Without this, if there was a gpgConfig, it would overwrite the initial
    values instead of merging them.

--- nix/gitconfig.nix
+++ nix/gitconfig.nix
@@ -1,31 +1,27 @@
 { lib, ... }:
 let 
-  gpgConfig = let
-    gpgSettings = lib.optionalAttrs (builtins.pathExists ./git-gpg.nix) (import ./git-gpg.nix);
-  in
-  {
-    inherit gpgSettings;
+  baseConfig = {
+    user = {
+      email = "rstefanic72@gmail.com";
+      name = "Robert Stefanic";
+    };
+    alias = {
+      l = "!. ~/.scripts/githelpers && pretty_git_log";
+      la = "!git l --all";
+      lr = "!git l -30";
+      lra = "!git lr --all";
+      lg = "!git l -G $1 -- $2";
+    };
+    core = {
+      editor = "nvim";
+    };
+    diff = {
+      algorithm = "histogram";
+    };
+    commit = {
+      verbose = "true";
+    };
   };
+  gpgConfig = lib.optionalAttrs (builtins.pathExists ./git-gpg.nix) (import ./git-gpg.nix);
 in
-{
-  user = {
-    email = "rstefanic72@gmail.com";
-    name = "Robert Stefanic";
-  };
-  alias = {
-    l = "!. ~/.scripts/githelpers && pretty_git_log";
-    la = "!git l --all";
-    lr = "!git l -30";
-    lra = "!git lr --all";
-    lg = "!git l -G $1 -- $2";
-  };
-  core = {
-    editor = "nvim";
-  };
-  diff = {
-    algorithm = "histogram";
-  };
-  commit = {
-    verbose = "true";
-  };
-} // gpgConfig
+lib.recursiveUpdate baseConfig gpgConfig
