4483125f7f2511675b402010ae702d1ec4bab0a9
Robert Stefanic <rstefanic72@gmail.com>
2025-04-05 15:26:48 -0400

    Replace `install.sh` with Nix home-manager
    Symlinking the dotfiles and installing tpm now occur in home-manager.

--- nix/home.nix
+++ nix/home.nix
@@ -10,6 +10,23 @@ in
     ];
   };
 
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    plugins = [
+      (pkgs.tmuxPlugins.mkTmuxPlugin {
+        pluginName = "tpm";
+        name = "tpm";
+        src = pkgs.fetchFromGitHub {
+          owner = "tmux-plugins";
+          repo = "tpm";
+          rev = "99469c4a9b1ccf77fade25842dc7bafbc8ce9946";
+          hash = "sha256-hW8mfwB8F9ZkTQ72WQp/1fy8KL1IIYMZBtZYIwZdMQc=";
+        };
+      })
+    ];
+  };
+
   home.packages = with pkgs; [
     _1password-cli
     cargo
