d55c25c2fa53103cf8b6ba75da2df41c0d7f9d6b
Robert Stefanic <rstefanic72@gmail.com>
2025-05-14 09:21:56 -0400

    nvim: Nord colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
   {
-    'Alligator/accent.vim',
+    'shaunsingh/nord.nvim',
     priority = 1000,
     config = function()
-      vim.g.accent_colour = 'blue'
-      vim.cmd.colorscheme 'accent'
-
-      -- Remove different SignColumn background color
-      vim.api.nvim_set_hl(0, 'SignColumn', { guibg = NONE })
+      vim.g.nord_italic = false
+      vim.cmd.colorscheme 'nord'
     end,
   }
 }
