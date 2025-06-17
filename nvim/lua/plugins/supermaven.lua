d8a9cfdbc2842f1763de64169f17af4bf0147726
Robert Stefanic <rstefanic72@gmail.com>
2025-06-16 22:40:35 -0400

    nvim: Customize supermaven keymaps

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -2,7 +2,13 @@ return {
   {
     "supermaven-inc/supermaven-nvim",
     config = function()
-      require("supermaven-nvim").setup({})
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-j>",
+          accept_word = "<C-k>",
+          clear_suggestion = "<C-]>",
+        },
+      })
     end,
   },
 }
