b647d33ba88a4224a537fa3b62b5ad976e0f2f62
Robert Stefanic <rstefanic72@gmail.com>
2025-09-29 07:29:12 -0400

    nvim: Everforest colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,16 @@ require 'config.lazy'
 require 'config.keymaps'
 require 'config.autocmds'
 
-vim.cmd [[colorscheme onehalfdark]]
+vim.g.everforest_background = 'hard'
+vim.cmd [[colorscheme everforest]]
+
+-- Overwrite Everforest spelling highlights to be less noisy
+vim.cmd([[
+    let s:configuration = everforest#get_configuration()
+    let s:palette = everforest#get_palette(s:configuration.background, s:configuration.colors_override)
+
+    call everforest#highlight('SpellBad', s:palette.red, s:palette.none)
+    call everforest#highlight('SpellCap', s:palette.blue, s:palette.none)
+    call everforest#highlight('SpellLocal', s:palette.aqua, s:palette.none)
+    call everforest#highlight('SpellRare', s:palette.purple, s:palette.none)
+]])
