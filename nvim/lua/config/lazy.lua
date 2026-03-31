d40c846637f950dfe57c4838b4e00554a6d2bdcf
Robert Stefanic <rstefanic72@gmail.com>
2026-03-31 01:20:41 -0400

    Neovim: Remove indent-blankline and update lazy-lock

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,15 +18,6 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
