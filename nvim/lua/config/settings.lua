33eb6132ce7746775611da79d2b554ba9ec8b221
Robert Stefanic <rstefanic72@gmail.com>
2026-04-05 21:18:20 -0400

    Neovim: Remove unnecessary settings

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -7,36 +7,30 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = false
 vim.opt.background = "light"
-vim.opt.hidden = true
-vim.opt.title = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.confirm = true
-vim.opt.updatetime = 250   -- Decrease CursorHold delay
-vim.opt.timeoutlen = 300
 vim.opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.showmode = false
 vim.opt.hlsearch = false
 vim.opt.signcolumn = 'yes'
-vim.opt.wildmode = 'longest:full,full'
 vim.opt.guicursor = "i:blockon1"
-vim.opt.spell = true
 
--- Tabs and what not
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- Tabs and test
+local tabwidth = 4
+vim.opt.tabstop = tabwidth
+vim.opt.softtabstop = tabwidth
+vim.opt.shiftwidth = tabwidth
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 vim.opt.cindent = true
 vim.opt.breakindent = true
-
--- Other text fun
+vim.opt.spell = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.wrap = false
@@ -48,6 +42,3 @@ vim.opt.fillchars = 'eob: '
 vim.opt.undofile = true
 vim.opt.backup = false
 vim.opt.swapfile = false
-
--- netrw
-vim.g.netrw_banner = 0
